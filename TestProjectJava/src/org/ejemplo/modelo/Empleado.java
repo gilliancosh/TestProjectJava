7189863c45871f4debf91273e0414632798584dd
bac_c <bac_c@BAC-PC-RNCT099>
2018-10-26 11:37:42 -0500

    Se agregaros getters and setters, Ultimo commit de este test....

--- TestProjectJava/src/org/ejemplo/modelo/Empleado.java
+++ TestProjectJava/src/org/ejemplo/modelo/Empleado.java
@@ -1,11 +1,50 @@
 package org.ejemplo.modelo;
 
+import java.util.Date;
+
 public class Empleado {
 	
 	private int id;
 	private String nombre;
+	private String apepat;
+	private String apemat;
+	private Date fecha;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApepat() {
+		return apepat;
+	}
+	public void setApepat(String apepat) {
+		this.apepat = apepat;
+	}
+	public String getApemat() {
+		return apemat;
+	}
+	public void setApemat(String apemat) {
+		this.apemat = apemat;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
 	
 	public static void main(String[] args) {
+		Empleado empleado = new Empleado();
+		empleado.setId(10);
 	
 		System.out.println("GitHub: Repository");
 	}
